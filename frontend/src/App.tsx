c7bf81de5ab7f9dd1d4024c92807d18f3a6cdbfc
Matthew Rosenberg <rosenberg.matthew@gmail.com>
2021-02-24 09:16:22 -0500

    revert

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,13 @@ import './App.css';
 import './components/navBar';
 import NavBar from './components/navBar';
 import BookGrid from './components/Book/bookGrid';
-
+import { isConstructorTypeNode } from 'typescript';
 
 function App() {
   return (
     <div className="App">
         <NavBar />
-        <BookGrid />
+          <BookGrid />
      </div>
   );
 }
